1d1020b5854d7323ee480419d8359bd8cf5a1f9b
Denny Biasiolli <denny.biasiolli@gmail.com>
2017-04-19 12:26:08 +0200

    adding models to admin

--- blood_donors/admin.py
+++ blood_donors/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    Sezione, CentroDiRaccolta, Sesso, StatoDonatore, TipoDonazione
+)
+
+
+class SezioneAdmin(admin.ModelAdmin):
+
+    # impostiamo come default l'utente corrente
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == 'owner':
+            kwargs['initial'] = request.user.id
+        return super(SezioneAdmin, self).formfield_for_foreignkey(db_field, request, **kwargs)
+
+
+class CentroDiRaccoltaAdmin(admin.ModelAdmin):
+
+    # impostiamo come default l'utente corrente
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == 'owner':
+            kwargs['initial'] = request.user.id
+        return super(CentroDiRaccoltaAdmin, self).formfield_for_foreignkey(db_field, request, **kwargs)
+
+
+class StatoDonatoreAdmin(admin.ModelAdmin):
+
+    # impostiamo come default l'utente corrente
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == 'owner':
+            kwargs['initial'] = request.user.id
+        return super(StatoDonatoreAdmin, self).formfield_for_foreignkey(db_field, request, **kwargs)
+
+admin.site.register(Sezione, SezioneAdmin)
+admin.site.register(CentroDiRaccolta, CentroDiRaccoltaAdmin)
+admin.site.register(Sesso)
+admin.site.register(StatoDonatore, StatoDonatoreAdmin)
+admin.site.register(TipoDonazione)
