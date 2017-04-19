e294f1fb9c2e05938a1ab56a66c3471d67df23d4
Denny Biasiolli <dennybiasiolli@users.noreply.github.com>
2017-04-19 11:13:48 +0200

    adding sezioni models
    `python manage.py makemigrations blood_donors --name add_sezioni`

--- blood_donors/models.py
+++ blood_donors/models.py
@@ -1,3 +1,22 @@
+from datetime import datetime
 from django.db import models
 
-# Create your models here.
+
+class Sezione(models.Model):
+    owner = models.ForeignKey('auth.User')
+    descrizione = models.CharField(max_length=255)
+    indirizzo = models.CharField(max_length=255, blank=True)
+    frazione = models.CharField(max_length=255, blank=True)
+    cap = models.CharField(max_length=10, blank=True)
+    citta = models.CharField(max_length=255, blank=True)
+    provincia = models.CharField(max_length=100, blank=True)
+    tel = models.CharField(max_length=255, blank=True)
+    fax = models.CharField(max_length=255, blank=True)
+    email = models.CharField(max_length=255, blank=True)
+
+    class Meta:
+        verbose_name_plural = "Sezioni"
+        unique_together = ('owner', 'descrizione',)
+
+    def __str__(self):
+        return self.descrizione
