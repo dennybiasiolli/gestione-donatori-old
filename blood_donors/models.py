5d73149f5d5ce8eeeb262cb3fa16dd90ff310c9b
Denny Biasiolli <dennybiasiolli@users.noreply.github.com>
2017-04-20 08:46:07 +0200

    adding donatore models
    `python manage.py makemigrations blood_donors --name add_donatori`

--- blood_donors/models.py
+++ blood_donors/models.py
@@ -81,3 +81,42 @@ class TipoDonazione(models.Model):
 
     def __str__(self):
         return self.descrizione
+
+
+class Donatore(models.Model):
+    sezione = models.ForeignKey('Sezione')
+    num_tessera = models.CharField(max_length=255)
+    num_tessera_cartacea = models.CharField(max_length=255, blank=True)
+    data_rilascio_tessera = models.DateField(null=True, blank=True)
+    cognome = models.CharField(max_length=255)
+    nome = models.CharField(max_length=255)
+    codice_fiscale = models.CharField(max_length=255, blank=True)
+    sesso = models.ForeignKey('Sesso')
+    data_nascita = models.DateField(null=True, blank=True)
+    data_iscrizione = models.DateField(null=True, blank=True)
+    stato_donatore = models.ForeignKey('StatoDonatore')
+    gruppo_sanguigno = models.CharField(max_length=10)
+    rh = models.CharField(max_length=10)
+    fenotipo = models.CharField(max_length=10, blank=True)
+    kell = models.CharField(max_length=10, blank=True)
+    indirizzo = models.CharField(max_length=255, blank=True)
+    frazione = models.CharField(max_length=255, blank=True)
+    cap = models.CharField(max_length=10, blank=True)
+    citta = models.CharField(max_length=255, blank=True)
+    provincia = models.CharField(max_length=100, blank=True)
+    tel = models.CharField(max_length=255, blank=True)
+    tel_lavoro = models.CharField(max_length=255, blank=True)
+    cell = models.CharField(max_length=255, blank=True)
+    fax = models.CharField(max_length=255, blank=True)
+    email = models.CharField(max_length=255, blank=True)
+    fermo_per_malattia = models.BooleanField(default=False)
+    donazioni_pregresse = models.IntegerField(default=0)
+    num_benemerenze = models.IntegerField(default=0)
+    centro_raccolta_default = models.ForeignKey('CentroDiRaccolta', blank=True, null=True)
+
+    class Meta:
+        verbose_name_plural = "Donatori"
+        unique_together = ('sezione', 'num_tessera',)
+
+    def __str__(self):
+        return self.num_tessera.upper() + ' ' + self.cognome.upper() + ' ' + self.nome.upper()
