492903a4025df3bf0e667e762fdb8a64be522a06
Denny Biasiolli <dennybiasiolli@users.noreply.github.com>
2020-09-28 09:26:04 +0200

    Revert "customizing token return value"
    This reverts previous commit.

--- website/urls.py
+++ website/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.authtoken import views
 from rest_framework.routers import DefaultRouter
 
 from donatori.urls import router as donatori_router
-from .views import CustomAuthToken
 
 
 router = DefaultRouter()
@@ -26,6 +26,6 @@ router.registry.extend(donatori_router.registry)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-token-auth/', CustomAuthToken.as_view()),
+    path('api-token-auth/', views.obtain_auth_token),
     path('api/', include(router.urls)),
 ]
