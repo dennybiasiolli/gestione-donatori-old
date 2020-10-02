b61401a88c007630b8afd1332a8f0899f712f5f7
Denny Biasiolli <dennybiasiolli@users.noreply.github.com>
2020-10-02 11:59:53 +0200

    displaying user_link in ProfiloUtenteAdmin

--- donatori/admin.py
+++ donatori/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
+from django.urls import reverse
+from django.utils.html import format_html
 
 from .models import ProfiloUtente
 
@@ -9,8 +11,15 @@ class ProfiloUtenteAdmin(admin.ModelAdmin):
     model = ProfiloUtente
     readonly_fields = ('utente',)
     list_display = ('utente', 'is_sezione', 'is_centro_di_raccolta',
-                    'is_donatore',)
+                    'is_donatore', 'user_link')
 
+    def user_link(self, obj):
+        return format_html(
+            '<a href="{}">{}</a>',
+            reverse('admin:auth_user_change', args=(obj.utente.pk,)),
+            'Modifica user',
+        )
+    user_link.short_description = 'Modifica Utente'
 
 
 class ProfiloUtenteInline(admin.StackedInline):
