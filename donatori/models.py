53cde817cc818adcb67f9732c63ab1647b30987c
Denny Biasiolli <dennybiasiolli@users.noreply.github.com>
2020-10-02 12:00:54 +0200

    adding Sezione model

--- donatori/models.py
+++ donatori/models.py
@@ -26,3 +26,26 @@ def create_or_update_user_profile(sender, instance, created, **kwargs):
     if created and ProfiloUtente.objects.filter(pk=instance.id).first() is None:
         ProfiloUtente.objects.create(utente=instance)
     instance.profiloutente.save()
+
+
+class Sezione(models.Model):
+    utente = models.OneToOneField(User, on_delete=models.CASCADE)
+    descrizione = models.CharField(max_length=255, unique=True)
+    ragione_sociale = models.CharField(max_length=255, blank=True)
+    indirizzo = models.CharField(max_length=255, blank=True)
+    frazione = models.CharField(max_length=255, blank=True)
+    cap = models.CharField(max_length=10, blank=True)
+    citta = models.CharField(max_length=255, blank=True)
+    provincia = models.CharField(max_length=100, blank=True)
+    tel = models.CharField(max_length=255, blank=True)
+    fax = models.CharField(max_length=255, blank=True)
+    email = models.CharField(max_length=255, blank=True)
+    presidente = models.CharField(max_length=255, blank=True)
+    segretario = models.CharField(max_length=255, blank=True)
+
+    class Meta:
+        verbose_name = 'Sezione'
+        verbose_name_plural = 'Sezioni'
+
+    def __str__(self):
+        return self.descrizione
