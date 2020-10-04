cb69643e3d69db1401fbe91c88ac9068c752b591
Denny Biasiolli <dennybiasiolli@users.noreply.github.com>
2020-10-04 15:23:00 +0200

    adding sessi api views with serializers and tests

--- donatori/tests/tests_api.py
+++ donatori/tests/tests_api.py
@@ -106,3 +106,84 @@ class SezioneViewSetTestCase(MockedTestCase):
         response = self.client.delete(
             '/api/sezioni/{}/'.format(self.sezione1.id))
         self.assertEqual(response.status_code, 405)
+
+
+class SessoViewSetTestCase(MockedTestCase):
+    def test_get_sessi(self):
+        self.client.force_login(self.avis1)
+        response = self.client.get('/api/sessi/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data), 2)
+        self.assertListEqual(response.data, [
+            {
+                'id': 1,
+                'codice': 'M',
+                'descrizione': 'Maschio',
+            },
+            {
+                'id': 2,
+                'codice': 'F',
+                'descrizione': 'Femmina',
+            },
+        ])
+
+    def test_get_sesso(self):
+        self.client.force_login(self.avis1)
+        response = self.client.get('/api/sessi/1/')
+        self.assertEqual(response.status_code, 200)
+        self.assertDictEqual(response.data, {
+            'id': 1,
+            'codice': 'M',
+            'descrizione': 'Maschio',
+            'gg_da_sangue_a_sangue': 90,
+            'gg_da_sangue_a_plasma': 30,
+            'gg_da_sangue_a_piastrine': 30,
+            'gg_da_plasma_a_sangue': 14,
+            'gg_da_plasma_a_plasma': 14,
+            'gg_da_plasma_a_piastrine': 14,
+            'gg_da_piastrine_a_sangue': 14,
+            'gg_da_piastrine_a_plasma': 30,
+            'gg_da_piastrine_a_piastrine': 30,
+        })
+        response = self.client.get('/api/sessi/2/')
+        self.assertEqual(response.status_code, 200)
+        self.assertDictEqual(response.data, {
+            'id': 2,
+            'codice': 'F',
+            'descrizione': 'Femmina',
+            'gg_da_sangue_a_sangue': 180,
+            'gg_da_sangue_a_plasma': 30,
+            'gg_da_sangue_a_piastrine': 30,
+            'gg_da_plasma_a_sangue': 14,
+            'gg_da_plasma_a_plasma': 14,
+            'gg_da_plasma_a_piastrine': 14,
+            'gg_da_piastrine_a_sangue': 14,
+            'gg_da_piastrine_a_plasma': 30,
+            'gg_da_piastrine_a_piastrine': 30,
+        })
+
+    def test_post_sesso(self):
+        self.client.force_login(self.avis1)
+        response = self.client.post('/api/sessi/')
+        self.assertEqual(response.status_code, 405)
+
+    def test_put_sesso(self):
+        self.client.force_login(self.avis1)
+        response = self.client.get('/api/sessi/')
+        id = response.data[0]['id']
+        response = self.client.put('/api/sessi/{}/'.format(id))
+        self.assertEqual(response.status_code, 405)
+
+    def test_patch_sesso(self):
+        self.client.force_login(self.avis1)
+        response = self.client.get('/api/sessi/')
+        id = response.data[0]['id']
+        response = self.client.patch('/api/sessi/{}/'.format(id))
+        self.assertEqual(response.status_code, 405)
+
+    def test_delete_sesso(self):
+        self.client.force_login(self.avis1)
+        response = self.client.get('/api/sessi/')
+        id = response.data[0]['id']
+        response = self.client.delete('/api/sessi/{}/'.format(id))
+        self.assertEqual(response.status_code, 405)
--- donatori/urls.py
+++ donatori/urls.py
@@ -3,6 +3,7 @@ from rest_framework.routers import DefaultRouter
 
 from .views import (
     CurrentUserViewSet,
+    SessoViewSet,
     SezioneViewSet,
 )
 
@@ -10,6 +11,7 @@ from .views import (
 router = DefaultRouter()
 router.register(r'me', CurrentUserViewSet)
 router.register(r'sezioni', SezioneViewSet)
+router.register(r'sessi', SessoViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
